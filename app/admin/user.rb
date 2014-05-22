746308127f93058d9ed18f4b25cbfff57d48563d
yzach <yossi.zach@gmail.com>
2014-05-22 15:07:31 +0300

    ActiveAdmin user should save all fields of the model

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -5,6 +5,7 @@ ActiveAdmin.register User do
     selectable_column
     id_column
     column :email
+    column :role
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -12,6 +13,7 @@ ActiveAdmin.register User do
   end
 
   filter :email
+  filter :role
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
@@ -27,4 +29,17 @@ ActiveAdmin.register User do
     f.actions
   end
 
+  controller do
+    def update
+      if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+        params[:user].delete("password")
+        params[:user].delete("password_confirmation")
+      end
+      super
+    end
+
+    def permitted_params
+       params.permit!
+    end
+  end
 end
